0defb20bd5f13cbf65172d506f47f3f334d8316e
Luis Eduardo <luise11@live.com.mx>
2022-09-11 23:22:30 -0500

    Desafio complementario 2 - Se reemplazo funcion por funcion flecha

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 //Realicé un programa con el fin de realizar una suma de la contratación de un paquete y multiples servicios, así como se ve en el HTML
 
 let sumaTotal=0
-
-function Sumar(precio){ //Función de Suma para la contratación de paquetes y servcios
-    sumaTotal = sumaTotal + precio
-    return sumaTotal;
-}
+let sumar = (precio) => sumaTotal += precio; //Función de Suma para la contratación de paquetes y servicios
 
 const paquetes = [
     {id:"P1", paquete:"Empresarial", precio:8000},
@@ -28,7 +24,7 @@ let paqueteIn = (prompt("Escoge el paquete que deseas (Escribir el ID del Paquet
 const paqueteCarrito = []; // Array nuevo en el que se almacenará el paquete que se desea contratar
 for (const paquete of paquetes){ // For necesario para recorrer el array y buscar el objeto perteneciente al ID ingresado 
     if(paqueteIn == paquete.id){// Si el ID es encontrado, se le sumará el precio correspondiente y se agregará el elemento al nuevo Array perteneciente a los servicios que se van a contratar
-        Sumar(paquete.precio)
+        sumar(paquete.precio)
         paqueteCarrito.push(paquete.paquete)
     }
 }
@@ -40,7 +36,7 @@ for (let index = 1; index <= numeroDeServicios; index++) { // Este "for" sirve p
     let servicioIn = (prompt("Escoge el servicio que deseas agregar (Escribir el ID del servicio) S1 - S7")) 
     for (const servicio of servicios){ // For necesario para recorrer el array y buscar el objeto perteneciente al ID ingresado
         if(servicioIn == servicio.id){ // Si el ID es encontrado, se le sumará el precio correspondiente y se agregará el elemento al nuevo Array perteneciente a los servicios que se van a contratar
-            Sumar(servicio.precio)
+            sumar(servicio.precio)
             serviciosCarrito.push(servicio.servicio)
         }
     }
